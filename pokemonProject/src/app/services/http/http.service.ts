6fe0c570cb0339253a59c3a4434b55d52211056b
amanda werner <amanda.werner@hotmail.com.br>
2020-03-26 19:17:33 -0300

    changed card style

--- pokemonProject/src/app/services/http/http.service.ts
+++ pokemonProject/src/app/services/http/http.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map, tap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { Pokemon } from 'src/app/classes/pokemon';
 import { environment } from 'src/environments/environment';
 
